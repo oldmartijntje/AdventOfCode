7434c4086c83de008e4561163df3bc991f8af90f
OldMartijntje_ubuntu <oldmartijntje@gmail.com>
2025-05-26 13:07:00 +0200

    2015/3/a

--- 2015/Day3/PuzzleA/main.js
+++ 2015/Day3/PuzzleA/main.js
@@ -1,2 +1,35 @@
 var tempPuzzleInput = puzzleInput;
 console.log(tempPuzzleInput);
+
+let x = 0;
+let y = 0;
+let presentLocations = {}
+presentLocations[`${x}.${y}`] = 1;
+
+for (let index = 0; index < tempPuzzleInput.length; index++) {
+    const element = tempPuzzleInput[index];
+    console.log(element)
+    switch (element) {
+        case "^":
+            y += 1;
+            break;
+        case "v":
+            y -= 1;
+            break;
+        case "<":
+            x -= 1;
+            break;
+        case ">":
+            x += 1;
+            break;
+        default:
+            alert("error")
+            break;
+    }
+    if (presentLocations[`${x}.${y}`] == undefined) {
+        presentLocations[`${x}.${y}`] = 1;
+    } else {
+        presentLocations[`${x}.${y}`] += 1;
+    }
+}
+alert(Object.keys(presentLocations).length)
