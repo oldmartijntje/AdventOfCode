b278ee6b7495699635518576c84fb061fc47c9d2
OldMartijntje_ubuntu <oldmartijntje@gmail.com>
2025-05-26 11:43:33 +0200

    2015/2/A

--- 2015/Day2/PuzzleA/main.js
+++ 2015/Day2/PuzzleA/main.js
@@ -1,2 +1,19 @@
 var tempPuzzleInput = puzzleInput;
-console.log(tempPuzzleInput)
+console.log(tempPuzzleInput);
+
+let rows = tempPuzzleInput.split('\n')
+let total = 0;
+rows.forEach(element => {
+    let dimensions = element.split('x');
+    dimensions[0] = Number(dimensions[0]);
+    dimensions[1] = Number(dimensions[1]);
+    dimensions[2] = Number(dimensions[2]);
+    let surfaces = [];
+    surfaces.push(dimensions[0] * dimensions[1] * 2);
+    surfaces.push(dimensions[2] * dimensions[1] * 2);
+    surfaces.push(dimensions[0] * dimensions[2] * 2);
+    let slack = Math.min(surfaces[0], surfaces[1], surfaces[2]) / 2;
+    // alert(`${slack}, ${dimensions}`)
+    total += slack + surfaces[0] + surfaces[1] + surfaces[2]
+});
+alert(total)
